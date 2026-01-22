36bee3192ed130cfcb4b93c139f2f95bae4d1c0c
Rahul kumar <131306879+rahulgit001@users.noreply.github.com>
2026-01-22 10:13:17 +0000

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/event-booking-app-frontend/', // 👈 VERY IMPORTANT
+  base: "/event-booking-app-frontend/",
   plugins: [react()],
 })
