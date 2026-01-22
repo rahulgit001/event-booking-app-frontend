c2d3cae7d2591513e5e25be323568e44b1776fff
Rahul kumar <131306879+rahulgit001@users.noreply.github.com>
2026-01-22 10:06:10 +0000

    Fix HashRouter duplicate import

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/event-booking-app-frontend/', // 👈 VERY IMPORTANT
   plugins: [react()],
-  base: '/event-booking-app-frontend/',
 })
